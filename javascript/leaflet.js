251ba6dc87ea6196ec3d59adbab4e3b680350cef
Jonathan.Auber <auber.jonathan@gmail.com>
2023-05-13 00:33:54 +0200

    Préparation pour merge

--- javascript/leaflet.js
+++ javascript/leaflet.js
@@ -1,19 +1,3 @@
-// function init(){
-//     const parc = {
-//         lat: 48.11384,
-//         lng: -1.669494
-//     }
-
-//     const zoomLevel = 7;
-
-//    const map = L.map('map').setView([parc.lat, parc.lng], zoomLevel);
-//    const mainLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// });
-
-// mainLayer.addTo(map);
-// }
 const mediterranean = {
     lat: 40,
     lng: 20
