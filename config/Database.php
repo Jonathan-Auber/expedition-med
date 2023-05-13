8c779d0eaa91d2ea2211c6bd8c78127916220b64
AdrienSchez <122347265+AdrienSchez@users.noreply.github.com>
2023-05-13 03:29:02 +0200

    weeeeeedodo
    aled

--- config/Database.php
+++ config/Database.php
@@ -10,7 +10,7 @@ class Database
     public static function getPdo(): PDO
     {
         if (self::$instance === NULL) {
-            self::$instance = new PDO('mysql:host=localhost;dbname=med;charset=utf8', 'root', 'root', [
+            self::$instance = new PDO('mysql:host=localhost;dbname=med;charset=utf8', 'root', '', [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ]);
