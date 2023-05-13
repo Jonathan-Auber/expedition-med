4af9a9144d2809b74c8ab51ab9bcb064a2b2284b
Jonathan.Auber <auber.jonathan@gmail.com>
2023-05-13 05:17:09 +0200

    Affichage des poi fonctionnel

---
+++ public/javascript/map.js
@@ -0,0 +1,54 @@
+var poi = []
+
+function convertDMSToDecimal(degrees, minutes, seconds, direction) {
+    // Calcul du degré décimal
+    let decimalDegrees = degrees + (minutes / 60) + (seconds / 3600);
+
+    // Vérification de la direction (N, S, E, W)
+    if (direction === 'S' || direction === 'W') {
+        decimalDegrees = -decimalDegrees;
+    }
+
+    return decimalDegrees;
+}
+
+
+fetch("users/data", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+}).then(function (response) {
+    return response.json();
+}).then(function (response) {
+    response.forEach(element => {
+        let latitude = element['Start Latitude'];
+        latitude = latitude.split(/[^\d\w]+/);
+        console.log(latitude);
+        let latitudeDecimal = convertDMSToDecimal(parseInt(latitude[0]), parseInt(latitude[1]), parseInt(latitude[2]), parseInt(latitude[3]));
+        let longitude = element['Start Longitude'];
+        longitude = longitude.split(/[^\d\w]+/);
+        console.log(longitude);
+        let longitudeDecimal = convertDMSToDecimal(parseInt(longitude[0]), parseInt(longitude[1]), parseInt(longitude[2]), parseInt(longitude[3]));
+        let position = [];
+        position = [latitudeDecimal, longitudeDecimal];
+        var marker = L.marker([latitudeDecimal, longitudeDecimal]);
+        marker.addTo(map);
+    });
+    // console.log(response);
+});
+
+// SPLIT
+
+const mediterranean = {
+    lat: 40,
+    lng: 20
+}
+
+const zoomLevel = 5;
+
+var map = L.map('map').setView([mediterranean.lat, mediterranean.lng], zoomLevel);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
